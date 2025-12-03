d9954fbdb184071c7c73d8f480dc78a80a05d534
KeingCasabona <keing.limache.casabona@gmail.com>
2025-12-02 23:24:16 -0500

    DataTypes2

--- Sección 2/02_datatypes.dart
+++ Sección 2/02_datatypes.dart
@@ -8,7 +8,32 @@ void main() {
   int _a = 30;
   double $b = 40;
 
-  print(a);
-  print(c);
-  print(_a + $b);
+  // print(a);
+  // print(c);
+  // print(_a + $b);
+
+  String nombre = 'Tony';
+  // String? nombre2;
+  String nombre2 = "Tony";
+
+  // print(nombre);
+  // print(null);
+  print(nombre2 == nombre);
+
+  String nombre3 = "O\'Connor";
+  String apellido = 'Stark';
+  String nombreCompleto = '$nombre $apellido';
+  print(nombreCompleto);
+
+  print(nombre3);
+
+  String multilinea = '''
+
+  Hola mundo
+  Como estas
+  o'Connor
+  $nombre2
+  ''';
+
+  print(multilinea);
 }
