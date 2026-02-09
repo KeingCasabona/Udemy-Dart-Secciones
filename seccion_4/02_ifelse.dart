8de1c7dfc2ee90eed0acbe6f47ffb90d65825fe8
KeingCasabona <keing.limache.casabona@gmail.com>
2026-02-08 20:58:31 -0500

    if y Else

---
+++ seccion_4/02_ifelse.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main() {
+  stdout.writeln('Cual es tu edad?');
+  int edad = int.parse(stdin.readLineSync() ?? '0');
+
+  stdout.writeln(edad);
+
+  if (edad >= 18) {
+    stdout.writeln('Eres mayor de edad');
+  } else {
+    stdout.writeln('Eres menor de edad');
+  }
+}
