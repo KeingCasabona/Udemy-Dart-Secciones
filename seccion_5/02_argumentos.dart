1b798088279d5412339ea7d3d8895f1fde336b57
KeingCasabona <keing.limache.casabona@gmail.com>
2026-03-03 20:49:32 -0500

    argumentos a las funciones-por nombre

--- seccion_5/02_argumentos.dart
+++ seccion_5/02_argumentos.dart
@@ -1,7 +1,13 @@
 void main(List<String> args) {
   saludar('HOla', 'Keing', 5);
+
+  saludar2('Saludos', veces: 20, nombre: 'Keing ');
 }
 
 void saludar(String mensaje, [String nombre = 'Hola', int edad = 20]) {
   print('$mensaje $nombre - $edad');
+}
+
+void saludar2(String? mensaje, {required String nombre, int veces = 10}) {
+  print('Saludar: $mensaje $nombre - $veces');
 }
