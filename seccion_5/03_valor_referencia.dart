d82d567abeabc1f85c18dbf4a5cbd17db6abddf5
KeingCasabona <keing.limache.casabona@gmail.com>
2026-03-03 23:20:08 -0500

    argumentos de valor y referencia

---
+++ seccion_5/03_valor_referencia.dart
@@ -0,0 +1,26 @@
+String capitalizar(String texto) {
+  texto = texto.toUpperCase();
+  return texto;
+}
+
+Map<String, String> capitalizarMapa(Map<String, String> mapa) {
+  //ROMPER LA REFERENCIA:
+  mapa = {...mapa};
+  mapa['nombre'] = mapa['nombre']?.toUpperCase() ?? 'No hay nombre';
+
+  return mapa;
+}
+
+main(List<String> args) {
+  String nombre = "keing";
+  String nombre2 = capitalizar(nombre);
+
+  print(nombre);
+  print(nombre2);
+
+  Map<String, String> persona = {'nombre': 'Tony Stark'};
+  Map<String, String> persona2 = capitalizarMapa(persona);
+
+  print(persona);
+  print(persona2);
+}
