72668262cf40243439916cc1f3b7556e595f83c6
KeingCasabona <keing.limache.casabona@gmail.com>
2026-03-06 22:51:14 -0500

    callbacks

--- seccion_5/04_flecha.dart
+++ seccion_5/04_flecha.dart
@@ -1,8 +1,8 @@
 main() {
-  int a = 10;
-  int b = 20;
+  // int a = 10;
+  // int b = 20;
 
-  int resultado = sumarFlecha(10, 20);
+  // int resultado = sumarFlecha(10, 20);
 
   List<int> listado = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 1];
 
