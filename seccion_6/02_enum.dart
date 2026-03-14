d18eeee989b53d04e718baac6bd63619619afc05
KeingCasabona <keing.limache.casabona@gmail.com>
2026-03-13 19:49:08 -0500

    Enum

---
+++ seccion_6/02_enum.dart
@@ -0,0 +1,23 @@
+main() {
+  // int volumen = 2; //0=volumen bajo; 1=volumen medio; 2=volumen alto
+
+  Audio volumen = Audio.bajo;
+
+  switch (volumen) {
+    case Audio.bajo:
+      print('Volumen bajo');
+      break;
+    case Audio.medio:
+      print('Volumen medio');
+      break;
+    case Audio.alto:
+      print('Volumen alto');
+      break;
+  }
+}
+
+enum Audio {
+  bajo,
+  medio,
+  alto,
+}
